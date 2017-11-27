cbd564aa87a761a2b7db98b66050c11cf26795df
Unknown <whatcharawali17@gmail.com>
2017-11-27 18:08:59 +0700

    ทำข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss Watcharawali koauk" << endl;
+  cout << "Nickname Jan" << endl;
+  cout << "Women" << endl;
+  cout << "8 Jul 1999" << endl;
+  cout << "Student" << endl;
+  cout << "Student" << endl;
   return 0;
 }
