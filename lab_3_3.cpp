66f6a800cafa4b66b424de3c54343242570579ec
Unknown <whatcharawali17@gmail.com>
2017-11-28 11:52:00 +0700

    ข้อที่ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -5,11 +5,21 @@ using namespace std;
 int main() {
     const float pi = 3.14315926536;
     float r;
+    float h=15;
+    float Area;
+    float Line;
+    float Cylider;
 
     cout << "please enter radius : ";
     cin >> r;
-    area = pi * r * r;
+    Area = pi * r * r;
     cout << "Area of circle : " << Area << endl;
+
+    Line = 2 * pi * r;
+    cout << "Line of circle : " << Line << endl;
+
+    Cylider = pi * r * h
+    cout << "Cylider of circle : " << Cylider << endl;
 
   return 0;
 }
